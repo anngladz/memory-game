a307c25a2bdbecc9082301c02c65bc0ce9a66958
Anna Gladzińska <agladzinska@gmail.com>
2020-04-14 17:29:09 +0200

    Add basic functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,57 @@
 const cards = document.querySelectorAll('.card');
 
+let hasFlippedCard = false;
+let lock = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle('flip');
+    if (lock) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
 }
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    reset();
+}
+
+function unflipCards() {
+    lock = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        reset();
+    }, 1200);
+}
+
+function reset() {
+    [hasFlippedCard, lock] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 16);
+        card.style.order = ramdomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
