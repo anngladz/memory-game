2fcab201361a1f0330e070a46b159f617c12fb79
Anna Gladzińska <agladzinska@gmail.com>
2020-04-17 18:12:29 +0200

    Add timer

--- js/script.js
+++ js/script.js
@@ -2,7 +2,31 @@ const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
 let lock = false;
+let start = false;
 let firstCard, secondCard;
+let time = 0;
+
+const step = () => {
+    time = time + 1;
+    document.querySelector('.timer').innerHTML = formatTime(time);
+}
+
+formatTime = (t) => {
+    const minutes = Math.floor(t / 60) % 60;
+    const seconds = (t % 60);
+    let formatedTime = (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+    return formatedTime;
+}
+
+const startTimer = () => {
+    id = setInterval(step, 1000);
+}
+
+const stopTimer = () => {
+    clearInterval(id);
+}
+
+startTimer();
 
 function flipCard() {
     if (lock) return;
@@ -24,6 +48,13 @@ function flipCard() {
 function checkForMatch() {
     let isMatch = firstCard.dataset.name === secondCard.dataset.name;
     isMatch ? disableCards() : unflipCards();
+    if (document.querySelectorAll('.card.flip').length == 16) {
+        setTimeout(function () {
+            stopTimer();
+            document.querySelector('.overlay').classList.add('display');
+            document.querySelector('.playertime').innerHTML = formatTime(time);
+        }, 500);
+    }
 }
 
 function disableCards() {
@@ -55,3 +86,7 @@ function reset() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+document.querySelector('.restart').addEventListener('click', function () {
+    window.location.reload(true);
+});
